6ccc5899eb54345bef633e89b152b3e267e35012
Giorgio <morellogiorgio327@gmail.com>
2026-06-09 12:35:09 -0300

    Fix database url & .env

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from typing import Annotated
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 engine = create_engine(
     os.getenv('DATABASE_URL'), # type: ignore
